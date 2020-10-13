a7c9d3883f0743ef979450a792e84832c8c7b5d5
Arnar Leifsson <arnarl@ru.is>
2020-10-13 09:56:59 +0000

    Update index.js

--- resolvers/index.js
+++ resolvers/index.js
@@ -3,14 +3,17 @@ const request = require("request");
 module.exports =  {
     Query: {
         allBasketballFields: async () => {
+            // This request function call needs to be returned - because it is a Promise and the resolvers knows how to resolve the promise, because it is async by nature
             request("https://basketball-fields.herokuapp.com/api/basketball-fields", (err, res, body) => {
-            console.log(body)
-            if (err) {
-                return err
-            }else{
-                console.log("Hello")
-                return JSON.parse(body);
-            }
+                // Here you are inside a callback function - which is not the same function as the outer function allBasketballFields
+                console.log(body)
+                if (err) {
+                    return err
+                } else {
+                    console.log("Hello")
+                    // This return only returns from the callback function - and request will return a promise
+                    return JSON.parse(body);
+                }
             })
         },
         basketballField: async (id) => {
